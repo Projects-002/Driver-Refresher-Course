9a94e12184e5fc9b7646875b22d042194228cd88
hopedevelopers254 <design.hopedevelopers@gmail.com>
2024-10-24 15:48:21 +0300

    updated

--- dashboard.php
+++ dashboard.php
@@ -47,7 +47,7 @@ include('database/db.php');
     <title>User Dashboard</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="container-fluid">
